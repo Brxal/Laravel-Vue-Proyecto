f699e26e1248b4e6367be36ab340d6733f461ec1
Brxal <torreslolitomanolo1@gmail.com>
2024-01-30 22:58:32 -0500

    se agrega el proyecto

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LessonController;
+use App\Http\Controllers\RoleController;
+
+
+//NO autch routes
+Route::get('/', [DashboardController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    //autch routes
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/lessons', LessonController::class);
+    Route::resource('/roles', RoleController::class);
+});
